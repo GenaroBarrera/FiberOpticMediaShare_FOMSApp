5e427525ad90b9eac2ab4dfaadeb5bc29a7610a8
Genaro Barrera <genarobarrera1995@gmail.com>
2025-12-15 20:07:58 -0600

    : Point the Client to the API

--- FOMSApp.Client/Program.cs
+++ FOMSApp.Client/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// POINT THIS TO YOUR API PORT (Check your terminal from Phase 1, likely 5083 or 5143)
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5083") });
 
 await builder.Build().RunAsync();
