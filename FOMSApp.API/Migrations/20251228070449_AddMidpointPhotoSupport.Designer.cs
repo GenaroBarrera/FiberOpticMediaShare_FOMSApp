050a516278606736809b62f0d366b534710d5e30
Genaro Barrera <genarobarrera1995@gmail.com>
2025-12-28 02:02:23 -0600

    create and run database migration to add photo support to midpoints

--- FOMSApp.API/Migrations/20251228070449_AddMidpointPhotoSupport.Designer.cs
+++ FOMSApp.API/Migrations/20251228070449_AddMidpointPhotoSupport.Designer.cs
@@ -4,6 +4,7 @@ using FOMSApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -12,9 +13,11 @@ using NetTopologySuite.Geometries;
 namespace FOMSApp.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251228070449_AddMidpointPhotoSupport")]
+    partial class AddMidpointPhotoSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FOMSApp.API/Migrations/20251228070449_AddMidpointPhotoSupport.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FOMSApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMidpointPhotoSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Vaults_VaultId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VaultId",
+                table: "Photos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MidpointId",
+                table: "Photos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_MidpointId",
+                table: "Photos",
+                column: "MidpointId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Midpoints_MidpointId",
+                table: "Photos",
+                column: "MidpointId",
+                principalTable: "Midpoints",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Vaults_VaultId",
+                table: "Photos",
+                column: "VaultId",
+                principalTable: "Vaults",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Midpoints_MidpointId",
+                table: "Photos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Vaults_VaultId",
+                table: "Photos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Photos_MidpointId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "MidpointId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VaultId",
+                table: "Photos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Vaults_VaultId",
+                table: "Photos",
+                column: "VaultId",
+                principalTable: "Vaults",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
